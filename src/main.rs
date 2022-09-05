2883d06342aa0542c14e9309150299dd102b01c9
ssnover <ssnover95@gmail.com>
2022-09-05 16:05:07 -0600

    Fmt

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,27 @@ const F_CPU: u32 = 16_000_000;
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let portb = peripherals.PORTB;
-    portb.ddrb.modify(|r, w| unsafe { w.bits(r.bits() | (1 << 5))});
+    portb
+        .ddrb
+        .modify(|r, w| unsafe { w.bits(r.bits() | (1 << 5)) });
 
     loop {
-        portb.portb.modify(|r, w| unsafe { w.bits(r.bits() | (1 << 5))});
-
-        portb.portb.modify(|r, w| unsafe { w.bits(r.bits() & !(1 << 5) )});
+        portb
+            .portb
+            .modify(|r, w| unsafe { w.bits(r.bits() | (1 << 5)) });
+        delay_ms(1000);
+        portb
+            .portb
+            .modify(|r, w| unsafe { w.bits(r.bits() & !(1 << 5)) });
+        delay_ms(1000);
     }
 }
 
 fn delay_ms(ms: u16) {
     let ticks = (F_CPU / 4000) as u16;
-    for _ in 0..(F_CPU / 4000) {
-        unsafe { asm!("nop", "nop", "nop", "nop"); }
+    for _ in 0..(F_CPU / 4000 * ms) {
+        unsafe {
+            asm!("nop", "nop", "nop", "nop");
+        }
     }
 }
